8d294399e5a1be2855562511f9c1905378581ca7
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-09-23 12:57:05 +0200

    update build script again

--- Build.ps1
+++ Build.ps1
@@ -56,8 +56,5 @@ exec { & dotnet restore }
 
 & Invoke-MSBuild
 
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
-
-exec { & dotnet pack .\src\Domain -c Release -o .\artifacts --version-suffix=$revision }
+exec { & dotnet pack .\src\Domain -c Release -o .\artifacts }
 exec { & dotnet test .\test\Domain.Tests -c Release }
