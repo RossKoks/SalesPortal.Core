44293480650bfd2d63a9ebd0b1d7a0f207d8e8a0
dominikus1993 <dominikus1910@gmail.com>
2016-09-24 11:51:18 +0200

    add simple implementation for abstract repository

--- src/Domain/Repositories/Abstractions/AbstractRepository.cs
+++ src/Domain/Repositories/Abstractions/AbstractRepository.cs
@@ -1,18 +1,66 @@
 ﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
 using Domain.Repositories.Interfaces;
+using SalesPortal.Core.Models;
 
 namespace Domain.Repositories.Abstractions
 {
     public abstract class AbstractRepository : IRepository
     {
-        public T Fill<T>(string connectionString, string procedureName, object obj)
+        public string ConnectionString { get; }
+
+        protected AbstractRepository(string baseConnectionString)
         {
-            throw new System.NotImplementedException();
+            ConnectionString = baseConnectionString;
         }
 
-        public IEnumerable<T> FillCollection<T>(string connectionString, string procedureName, object obj)
+        public Result<T> FillObject<T>(string connectionString, string procedureName, object obj) where T: class
         {
-            throw new System.NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var result = connection.Query<T>(procedureName, obj, commandType: CommandType.StoredProcedure)?.FirstOrDefault();
+                connection.Close();
+                return Result<T>.Wrap(result);
+            }
+        }
+
+        public Result<IEnumerable<T>> FillCollection<T>(string connectionString, string procedureName, object obj)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var result = connection.QueryMultiple(procedureName, obj, commandType: CommandType.StoredProcedure);
+                connection.Close();
+                return Result<IEnumerable<T>>.Wrap(result?.Read<T>());
+            }
+        }
+
+        public async Task<Result<T>> FillObjectAsync<T>(string connectionString, string procedureName, object obj) where T : class
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                var result = await connection.QueryAsync<T>(procedureName, obj, commandType: CommandType.StoredProcedure);
+                connection.Close();
+                return Result<T>.Wrap(result?.FirstOrDefault());
+            }
+        }
+
+        public async Task<Result<IEnumerable<T>>> FillCollectionAsync<T>(string connectionString, string procedureName, object obj)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                var queryResult = await connection.QueryMultipleAsync(procedureName, obj, commandType: CommandType.StoredProcedure);
+                var result = await (queryResult?.ReadAsync<T>() ?? Task.FromResult<IEnumerable<T>>(null));
+                connection.Close();
+                return Result<IEnumerable<T>>.Wrap(result);
+            }
         }
     }
 }
--- src/Domain/Repositories/Interfaces/IRepository.cs
+++ src/Domain/Repositories/Interfaces/IRepository.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SalesPortal.Core.Models;
 
 namespace Domain.Repositories.Interfaces
 {
     public interface IRepository
     {
-        T Fill<T>(string connectionString, string procedureName, object obj);
-        IEnumerable<T> FillCollection<T>(string connectionString, string procedureName, object obj);
+        Result<T> FillObject<T>(string connectionString, string procedureName, object obj) where T: class;
+        Result<IEnumerable<T>> FillCollection<T>(string connectionString, string procedureName, object obj);
+        Task<Result<T>> FillObjectAsync<T>(string connectionString, string procedureName, object obj) where T: class;
+        Task<Result<IEnumerable<T>>> FillCollectionAsync<T>(string connectionString, string procedureName, object obj);
     }
 }
