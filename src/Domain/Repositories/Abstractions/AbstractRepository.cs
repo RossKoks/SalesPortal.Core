1700c412221164849b9c69cd90a2847380ad1d28
dominikus1993 <dominikus1910@gmail.com>
2016-09-25 10:23:50 +0200

    remove using statments

--- src/Domain/Repositories/Abstractions/AbstractRepository.cs
+++ src/Domain/Repositories/Abstractions/AbstractRepository.cs
@@ -19,42 +19,36 @@ namespace Domain.Repositories.Abstractions
 
         public Result<T> FillObject<T>(string procedureName, object obj) where T : class
         {
-            using (var connection = _db.Connection)
-            {
-                var result = connection.Query<T>(procedureName, obj, commandType: CommandType.StoredProcedure)?.FirstOrDefault();
-                return Result<T>.Wrap(result);
-            }
+            var result =
+                _db.Connection.Query<T>(procedureName, obj, commandType: CommandType.StoredProcedure)?.FirstOrDefault();
+            return Result<T>.Wrap(result);
         }
 
         public Result<IEnumerable<T>> FillCollection<T>(string procedureName, object obj)
         {
-            using (var connection = _db.Connection)
+            using (
+                var result = _db.Connection.QueryMultiple(procedureName, obj, commandType: CommandType.StoredProcedure))
             {
-                using (var result = connection.QueryMultiple(procedureName, obj, commandType: CommandType.StoredProcedure))
-                {
-                    return Result<IEnumerable<T>>.Wrap(result?.Read<T>());
-                }
+                return Result<IEnumerable<T>>.Wrap(result?.Read<T>());
             }
         }
 
         public async Task<Result<T>> FillObjectAsync<T>(string procedureName, object obj) where T : class
         {
-            using (var connection = _db.Connection)
-            {
-                var result = await connection.QueryAsync<T>(procedureName, obj, commandType: CommandType.StoredProcedure);
-                return Result<T>.Wrap(result?.FirstOrDefault());
-            }
+            var result =
+                await _db.Connection.QueryAsync<T>(procedureName, obj, commandType: CommandType.StoredProcedure);
+            return Result<T>.Wrap(result?.FirstOrDefault());
         }
 
         public async Task<Result<IEnumerable<T>>> FillCollectionAsync<T>(string procedureName, object obj)
         {
-            using (var connection = _db.Connection)
+            using (
+                var reader =
+                    await
+                        _db.Connection.QueryMultipleAsync(procedureName, obj, commandType: CommandType.StoredProcedure))
             {
-                using (var reader = await connection.QueryMultipleAsync(procedureName, obj, commandType: CommandType.StoredProcedure))
-                {
-                    var result = await (reader?.ReadAsync<T>() ?? Task.FromResult<IEnumerable<T>>(null));
-                    return Result<IEnumerable<T>>.Wrap(result);
-                }
+                var result = await (reader?.ReadAsync<T>() ?? Task.FromResult<IEnumerable<T>>(null));
+                return Result<IEnumerable<T>>.Wrap(result);
             }
         }
 
